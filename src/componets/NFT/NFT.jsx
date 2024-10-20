7ec6b509d1193b64abc7d53b90a382665bec7b1c
Hemant Kumar <96202628+hemantkr1412@users.noreply.github.com>
2024-10-20 19:13:52 +0530

    Update NFT.jsx

--- src/componets/NFT/NFT.jsx
+++ src/componets/NFT/NFT.jsx
@@ -117,7 +117,7 @@ const NFT = () => {
 
 
     const priceInEth = "1"; // Assuming the price is 0.1 AMB
-    const nftContractAddress = "0xc0225f41138bA992AA857F8F56DCBFe8eF7d13c8"; // Replace with your contract address
+    const nftContractAddress = "0xb81562A64A6f1CDA748f88DAb3410d2840346413"; // Replace with your contract address
     const totalSupply = "10";
 
     const { account, provider, signer, chainId } = useContext(AccountContext);
